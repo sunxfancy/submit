080958ea0b21284dbcd037791cd21be98dfd2dee
sunxfancy <sunxfancy@gmail.com>
2018-01-10 17:03:15 -0800

    first version

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+/**
+* @Author: Sun Xiaofan <sxf>
+* @Date:   2016-11-14
+* @Email:  sunxfancy@gmail.com
+* @Last modified by:   sxf
+* @Last modified time: 2016-11-14
+* @License: MIT License
+*/
+
+
+#include <iostream>
+#include "cmdline.hpp"
+
+
+int main(int argc, char const *argv[]) {
+    cmdline::parser parser;
+    parser.add("show", 's', "Show working queue now");
+
+    parser.set_program_name("submit");
+    parser.parse_check(argc, argv);
+    if (parser.rest().size() == 0)
+    {
+        std::cerr << parser.usage();
+        exit(0);
+    }
+
+    
+
+    return 0;
+}
